f20568616528e461cfb707416f12705559357bd6
Caroline Thomsen <caro143e@stud.kea.dk>
2023-02-20 09:25:20 +0100

    final

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
+    
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
